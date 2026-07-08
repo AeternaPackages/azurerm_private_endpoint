7bd3f80be62a3394b801e277099e213fc49dbc79
Daniel Allen <dan@aeternagroup.net>
2026-07-08 23:41:06 +0100

    Update to schema version v4.80.0

---
+++ main.tf
@@ -0,0 +1,24 @@
+locals {
+  private_endpoints = { for k1, v1 in var.private_endpoints : k1 => { custom_network_interface_name = v1.custom_network_interface_name, ip_configuration = v1.ip_configuration, location = v1.location, name = v1.name, private_dns_zone_group = v1.private_dns_zone_group, private_service_connection = v1.private_service_connection, resource_group_name = v1.resource_group_name, subnet_id = v1.subnet_id, tags = v1.tags } }
+
+  private_endpoint_application_security_group_associations = merge([
+    for k1, v1 in var.private_endpoints : {
+      for k2, v2 in coalesce(v1.private_endpoint_application_security_group_associations, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        private_endpoint_id = module.private_endpoints.private_endpoints["${k1}"].id
+      })
+    }
+  ]...)
+}
+
+module "private_endpoints" {
+  source            = "git::https://github.com/AeternaModules/azurerm_private_endpoint.git?ref=v4.80.0"
+  private_endpoints = local.private_endpoints
+}
+
+module "private_endpoint_application_security_group_associations" {
+  source                                                   = "git::https://github.com/AeternaModules/azurerm_private_endpoint_application_security_group_association.git?ref=v4.80.0"
+  private_endpoint_application_security_group_associations = local.private_endpoint_application_security_group_associations
+  depends_on                                               = [module.private_endpoints]
+}
+
---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "private_endpoints" {
+  description = <<EOT
+Map of private_endpoints, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+    - subnet_id
+    - private_service_connection (block)
+Optional:
+    - custom_network_interface_name
+    - tags
+    - ip_configuration (block)
+    - private_dns_zone_group (block)
+Nested private_endpoint_application_security_group_associations (azurerm_private_endpoint_application_security_group_association):
+    Required:
+        - application_security_group_id
+EOT
+
+  type = map(object({
+    location                      = string
+    name                          = string
+    resource_group_name           = string
+    subnet_id                     = string
+    custom_network_interface_name = optional(string)
+    tags                          = optional(map(string))
+    private_service_connection = object({
+      is_manual_connection              = bool
+      name                              = string
+      private_connection_resource_alias = optional(string)
+      private_connection_resource_id    = optional(string)
+      request_message                   = optional(string)
+      subresource_names                 = optional(list(string))
+    })
+    ip_configuration = optional(object({
+      member_name        = optional(string)
+      name               = string
+      private_ip_address = string
+      subresource_name   = optional(string)
+    }))
+    private_dns_zone_group = optional(object({
+      name                 = string
+      private_dns_zone_ids = list(string)
+    }))
+    private_endpoint_application_security_group_associations = optional(map(object({
+      application_security_group_id = string
+    })))
+  }))
+
+  validation {
+    condition = alltrue(concat(
+      [for kk in keys(var.private_endpoints) : !strcontains(kk, "/")],
+      flatten([for k0, v0 in var.private_endpoints : [for kk in keys(coalesce(v0.private_endpoint_application_security_group_associations, {})) : !strcontains(kk, "/")]])
+    ))
+    error_message = "Map keys in this package must not contain '/': it is used internally as a nesting-key separator, so a key containing it can silently collide two different nested entries into one. Rename the offending key(s)."
+  }
+}
